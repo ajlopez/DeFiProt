098a0dc87d4772ae485e1b3d66528a2ec5cdc608
ajlopez <webmaster@ajlopez.com>
2020-02-23 13:18:40 -0300

    Market cannot mint without enough tokens

--- test/Market_tests.js
+++ test/Market_tests.js
@@ -2,6 +2,8 @@
 const Market = artifacts.require('./Market.sol');
 const Token = artifacts.require('./test/FaucetToken.sol');
 
+const expectThrow = require('./utils').expectThrow;
+
 contract('Market', function (accounts) {
     const alice = accounts[0];
     const bob = accounts[1];
@@ -34,6 +36,21 @@ contract('Market', function (accounts) {
         const totalSupply = await this.market.totalSupply();
         
         assert.equal(totalSupply, 1000);
+    });
+    
+    it('cannot mint amount without enough tokens', async function () {
+        await this.token.approve(this.market.address, 500, { from: alice });
+        expectThrow(this.market.mint(1000, { from: alice }));
+        
+        const aliceBalance = await this.market.balanceOf(alice);
+        const bobBalance = await this.market.balanceOf(bob);
+        
+        assert.equal(aliceBalance, 0);
+        assert.equal(bobBalance, 0);
+        
+        const totalSupply = await this.market.totalSupply();
+        
+        assert.equal(totalSupply, 0);
     });
 });
 
