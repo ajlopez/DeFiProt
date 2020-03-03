b1b85880bd2cb6a9ad6fb8c2b95fbd326b1b69b4
ajlopez <webmaster@ajlopez.com>
2020-03-03 13:00:12 -0300

    Controller account liquidity using deposits in markets

--- contracts/Controller.sol
+++ contracts/Controller.sol
@@ -34,6 +34,19 @@ contract Controller {
         marketList.push(market);
     }
     
+    function getAccountLiquidity(address account) public view returns (uint) {
+        uint liquidity = 0;
+        
+        for (uint k = 0; k < marketList.length; k++) {
+            MarketInterface market = MarketInterface(marketList[k]);
+            uint price = prices[marketList[k]];
+            liquidity += market.balanceOf(account) * price;
+            liquidity -= market.borrowedBy(account) * 2 * price;
+        }
+        
+        return liquidity;
+    }
+    
     function lock(address market, address user, uint amount) external onlyMarket {
         require(markets[market]);
         
--- test/Controller_tests.js
+++ test/Controller_tests.js
@@ -8,6 +8,7 @@ const expectThrow = require('./utils').expectThrow;
 contract('Controller', function (accounts) {
     const alice = accounts[0];
     const bob = accounts[1];
+    const charlie = accounts[2];
 
     describe('one token and one market', function () {
         beforeEach(async function() {
@@ -72,6 +73,49 @@ contract('Controller', function (accounts) {
             const result = await this.controller.prices(this.token.address);
             
             assert.equal(result, 0);
+        });
+    });
+    
+    describe('one token and one market', function () {
+        beforeEach(async function() {
+            this.token = await Token.new(1000000, "Token", 0, "TOK");
+            this.token2 = await Token.new(1000000, "Token 2", 0, "TOK2");
+            
+            await this.token.allocateTo(bob, 1000000);
+            await this.token2.allocateTo(bob, 1000000);
+            
+            await this.token.allocateTo(charlie, 1000000);
+            await this.token2.allocateTo(charlie, 1000000);
+            
+            this.market = await Market.new(this.token.address);
+            this.market2 = await Market.new(this.token2.address);            
+          
+            this.controller = await Controller.new();
+            await this.controller.addMarket(this.market.address);
+            await this.controller.addMarket(this.market2.address);
+            
+            await this.controller.setPrice(this.market.address, 10);
+            await this.controller.setPrice(this.market2.address, 20);
+        });
+        
+        it('account liquidity using deposits', async function () {
+            const result = await this.controller.getAccountLiquidity(alice);
+            
+            assert.equal(result, 0);
+            
+            await this.token.approve(this.market.address, 100, { from: alice });
+            await this.market.mint(100, { from: alice });
+            
+            const result2 = await this.controller.getAccountLiquidity(alice);
+            
+            assert.equal(result2, 100 * 10);
+            
+            await this.token2.approve(this.market2.address, 100, { from: alice });
+            await this.market2.mint(100, { from: alice });
+            
+            const result3 = await this.controller.getAccountLiquidity(alice);
+            
+            assert.equal(result3, 100 * 10 + 100 * 20);
         });
     });
 });
