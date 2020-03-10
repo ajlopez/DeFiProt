f2c0070fd5e7d7f91bf7c602dd4147c6c64b6484
ajlopez <webmaster@ajlopez.com>
2020-03-10 17:02:46 -0300

    Improved deploy command

--- commands/deploy.cmd
+++ commands/deploy.cmd
@@ -1,17 +1,17 @@
 node deploy root controller Controller
 
 node deploy root market1 Market token1;1000
-node invoke root controller addMarket(address) market1
-node invoke root market1 setController(address) controller
-node invoke root controller setPrice(address,uint256) market1;10
+node invoke root controller addMarket(address) market1 fast
+node invoke root market1 setController(address) controller fast
+node invoke root controller setPrice(address,uint256) market1;10 fast
 
 node deploy root market2 Market token2;1000
-node invoke root controller addMarket(address) market2
-node invoke root market2 setController(address) controller
-node invoke root controller setPrice(address,uint256) market2;20
+node invoke root controller addMarket(address) market2 fast
+node invoke root market2 setController(address) controller fast
+node invoke root controller setPrice(address,uint256) market2;20 fast
 
 node deploy root market3 Market token3;1000
-node invoke root controller addMarket(address) market3
-node invoke root market3 setController(address) controller
-node invoke root controller setPrice(address,uint256) market3;30
+node invoke root controller addMarket(address) market3 fast
+node invoke root market3 setController(address) controller fast
+node invoke root controller setPrice(address,uint256) market3;30 fast
 
