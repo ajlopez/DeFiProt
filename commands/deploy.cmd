d9db1819ed529232dcbf4ee15c96328021253192
ajlopez <webmaster@ajlopez.com>
2020-03-10 15:54:28 -0300

    Improved commands

--- commands/deploy.cmd
+++ commands/deploy.cmd
@@ -1,18 +1,17 @@
-node deploy root market1 Market token1
-node deploy root market2 Market token2
-node deploy root market3 Market token3
-
 node deploy root controller Controller
 
+node deploy root market1 Market token1;1000
 node invoke root controller addMarket(address) market1
-node invoke root controller addMarket(address) market2
-node invoke root controller addMarket(address) market3
-
 node invoke root market1 setController(address) controller
-node invoke root market2 setController(address) controller
-node invoke root market3 setController(address) controller
-
 node invoke root controller setPrice(address,uint256) market1;10
+
+node deploy root market2 Market token2;1000
+node invoke root controller addMarket(address) market2
+node invoke root market2 setController(address) controller
 node invoke root controller setPrice(address,uint256) market2;20
+
+node deploy root market3 Market token3;1000
+node invoke root controller addMarket(address) market3
+node invoke root market3 setController(address) controller
 node invoke root controller setPrice(address,uint256) market3;30
 
--- commands/invoke.js
+++ commands/invoke.js
@@ -10,11 +10,16 @@ const from = utils.getAccount(config, process.argv[2]);
 const to = utils.getInstanceAddress(config, process.argv[3]);
 const fn = process.argv[4];
 let args = utils.getArguments(config, process.argv[5]);
+const fast = process.argv[6] === 'fast';
 
 (async function() {
     try {
         const txh = await client.invoke(from, to, fn, args);
         console.log('transaction', txh);
+        
+        if (fast)
+            return;
+        
         const txr = await client.receipt(txh, 0);
         console.log(txr && parseInt(txr.status) ? 'done' : 'failed');
     }
